ae40ba611a3f2e33859cfa45257f5ccb64a8279c
Mostafa Sakr <msakr3@gmail.com>
2022-08-30 15:24:18 -0600

    Test: modify test to check each method individually.

--- spec/row_spec.rb
+++ spec/row_spec.rb
@@ -16,16 +16,52 @@ RSpec.describe Row do
 
         expect(row.occupied). to eq(FALSE)      #Initial state of attributes should be FALSE
 
+        expect(row.player1).to eq(TRUE)         #Player1 = TRUE represents player 1
+
+    end
+
+    xit "has a method to toggle occupied attribute to TRUE" do
+
+        row = Row.new
+
+        expect(row.occupied). to eq(FALSE)      #Initial state of attributes should be FALSE
+
         expect(row.player1).to eq(TRUE)         #Defaults to player1
+
 
         row.toggle_occupied                      #Calling the toggle method on the occupied attribute
 
-        row.toggle_player1                       #Calling the toggle method on the player1 attribute
-
-        expect(row.occupied). to eq(TRUE)
-
-        expect(row.player1).to eq(TRUE)
+        expect(row.occupied). to eq(TRUE)   
 
     end
+
+    xit "it has a method to toggle player1 attribute to false" do
+
+        row = Row.new
+
+        row.toggle_player1
+
+        expect(row.occupied). to eq(TRUE)      #Initial state of attributes should be FALSE
+    
+        expect(row.player1).to eq(FALSE)        #Player1 = FALSE represents player 2
+
+    end
+
+    xit "toggle_player1 also makes row occupied" do
+
+        row = Row.new
+
+        row.toggle_occupied
+        
+        row.toggle_player1
+
+        expect(row.occupied). to eq(FALSE)     #Game Logic-wise this makes no difference from occupied = FALSE and player1 =TRUE
+    
+        expect(row.player1).to eq(FALSE)        
+
+
+    end
+
+
 
 end
