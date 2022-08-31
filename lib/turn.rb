16173700300e8b0b6ea724722a999a7af163c208
Bryan Keener <bkeener7@gmail.com>
2022-08-31 17:45:22 -0600

    Add test/turn branch with associated files

---
+++ lib/turn.rb
@@ -0,0 +1,7 @@
+# needs to have player select column (user input) only A-G, otherwise error
+# needs to show invalid plays (a full column), decide where this occurs
+# program bot here (maybe)
+# Board starts top left at columns[0] and rows[0] and bottom right is columns[6] and rows[5]
+# print_board after every turn
+# incorporate a draw, win and loss
+# look at iteration 3 gameplay and end game
