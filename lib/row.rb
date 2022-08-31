25b6e93e515f5a043b731902b6752b0aafd087bf
Bryan Keener <bkeener7@gmail.com>
2022-08-30 19:17:19 -0600

    Add toggle_playable method

--- lib/row.rb
+++ lib/row.rb
@@ -1,13 +1,18 @@
 class Row
-    attr_reader :player, :occupied
+    attr_reader :player, :occupied, :playable
 
     def initialize(player = "")
         @player = player
         @occupied = false
+        @playable = false
     end
 
     def toggle_occupied
         @occupied = !@occupied
+    end
+
+    def toggle_playable
+        @playable = !@playable
     end
     
 end
