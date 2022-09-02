8fa0ede0df8f636f469411d168bb86d8e49c1631
Mostafa Sakr <msakr3@gmail.com>
2022-09-02 09:20:54 -0600

    Correct calling player_1 and player_2 attributes in some a test and in class file.

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,16 @@
 class Board
 
   attr_reader :columns,
-              :player1,
-              :player2,
+              :player_1,
+              :player_2,
               :column_index,
               :row_index,
               :layout_index,
               :layout
 
-  def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2 
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2 
     @columns = []
     generate_columns
     @column_index = 0
@@ -19,7 +19,6 @@ class Board
     @layout = "\n\nA B C D E F G\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n\n"
   end
 
-
   def generate_columns
     7.times do
       @columns << Column.new
@@ -27,57 +26,39 @@ class Board
   end
 
   def layout_print
-
     print @layout
-
-    #column[0].row[0] is layout[16]
-    #column[6].row[0] is layout[28]
-    #column[0].row[1] is layout[30]
-
   end
 
   def update_layout
-
     board_logic_loop
-
     @column_index = 0
     @row_index = 0
     @layout_index = 16
-
   end
 
   def board_logic
-
     if @columns[@column_index].rows[@row_index].player == @player1
       @layout[@layout_index] = "x"
     elsif @columns[@column_index].rows[@row_index].player == @player2
       @layout[@layout_index] = "o"
     end
-
-
   end
 
   def board_logic_loop
-
     42.times do
-
       if @column_index == 7
         @column_index = 0
         @row_index += 1
       end
-
      board_logic
-
      @column_index += 1
      @layout_index += 2
-
     end
-
   end
-
-
-
 end
 
-#generate board on this file and make appropriate tests
+#Board array to string reference key:
+#column[0].row[0] is layout[16]
+#column[6].row[0] is layout[28]
+#column[0].row[1] is layout[30]
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,24 +8,20 @@ require 'pry'
 RSpec.describe Board do
 
     it "1. exists" do
-
     	board = Board.new("Megan", "Hod")
-      expect(board).to be_an_instance_of(Board)
 
+      expect(board).to be_an_instance_of(Board)
     end
 
     it "2. has 2 players which are dynamic attributes" do
 			board = Board.new("Megan", "Hod")
-			expect(board.player1).to eq("Megan")
-			expect(board.player2).to eq("Hod")
+
+			expect(board.player_1).to eq("Megan")
+			expect(board.player_2).to eq("Hod")
 		end
 
-
-
     it "3. has a readable array attribute composed of 7 column instances with 6 row instances each" do
-
       board = Board.new("Megan", "Hod")
-
 
       expect(board.columns.length).to eq(7)
       expect(board.columns[0]).to be_an_instance_of(Column)
@@ -33,42 +29,46 @@ RSpec.describe Board do
       expect(board.columns[0].rows.length).to eq(6)
       expect(board.columns[0].rows[0]).to be_an_instance_of(Row)
       expect(board.columns[6].rows[5]).to be_an_instance_of(Row)
-
     end
 
+    it "4. has a column index attribute with a default value of 0" do
+      board = Board.new("Megan", "Hod")
 
+      expect(board.column_index).to eq(0)
+    end
 
+    it "5. has a row index attribute with a default value of 0" do
+      board = Board.new("Megan", "Hod")
+
+      expect(board.row_index).to eq(0)
+    end
+
+    it "6. has a layout index for the string with a default value of 16" do
+      board = Board.new("Megan", "Hod")
+
+      expect(board.layout_index).to eq(16)
+    end
 
     it ". prints out a board of 7 columns, A through G, and 6 rows." do
-
         board = Board.new("Megan", "Hod")
-
 
         expect(board.columns.length).to eq(7)
         expect(board.columns[0].rows.length).to eq(6)
-
-
-
         expect(board.layout).to eq("\n\nA B C D E F G\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n\n")
 				expect(board.layout_print).to eq(print "\n\nA B C D E F G\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n\n")
-
     end
 
 
 
-    it ". changes dots on the board to 'x' for player 1 or 'o' for player 2" do
-
+    xit ". changes dots on the board to 'x' for player 1 or 'o' for player 2" do
         board = Board.new("Megan", "Hod")
   
         expect(board.columns.length).to eq(7)
         expect(board.columns[0].rows.length).to eq(6)
-
-
         expect(board.layout).to eq("\n\nA B C D E F G\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n\n")
 
         board.columns[0].play_piece("Megan")
-        expect(board.columns[0].rows[5].player).to eq("Megan")
-
+        expect(board.columns[0].rows[5].player_1).to eq("Megan")
 
         board.update_layout
         expect(board.layout).to eq("\n\nA B C D E F G\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\nx . . . . . .\n\n")
@@ -122,9 +122,6 @@ RSpec.describe Board do
 
         board.update_layout
         expect(board.layout).to eq("\n\nA B C D E F G\nx . . . . . .\nx . . . . . o\nx . . . . . o\nx . . . . . o\nx . . . . . o\nx . . x o . o\n\n")
-
-
-
     end
 
 
