23d50e63e68807e6a3f7a85bc6c46f55298b5f62
Mostafa Sakr <msakr3@gmail.com>
2022-09-05 00:20:03 -0600

    Add player turns, modify some methods and start code for game sequence.

--- lib/game.rb
+++ lib/game.rb
@@ -10,6 +10,8 @@ class Game
     @board = Board.new
     @is_bot = false
     @bot_choices = ["A", "B", "C", "D", "E", "F", "G"]
+    @player_1_turn = ""
+    @player_2_turn = ""
   end
 
   def set_player1
@@ -39,21 +41,25 @@ class Game
   def welcome_message
     "\nWelcome to CONNECT FOUR\nEnter p to play. Enter q to quit.\n"
   end
-  #^there due to limitation on testing methods with user input.
-
-  def main_menu
-    print welcome_message
-    main_menu_user_input
-  end
 
   def main_menu_user_input
     if gets.chomp == "q"
       abort ":("
     elsif gets.chomp == "p"
       set_player1
+      set_bot
     else
       print "\nSorry, please try again.\n"
     end
+  end
+
+  # a = ("a" if foo) || ("b" if bar) || "c"
+
+  # if foo 'a' elsif bar 'b' else 'c' syntax to help me refactor later -Mostafa
+
+  def main_menu
+    print welcome_message
+    main_menu_user_input
   end
 
   def main_menu_loop
@@ -62,11 +68,26 @@ class Game
     end
   end
 
+  def player_turns
+    @player_1_turn = Turn.new(@player_1, @board)
+    @player_2_turn = Turn.new(@player_2, @board)
+  end
+  
+  def game_sequence
+    loop do
+      @player_1_turn.column_select(gets.chomp)
+      #check win conditions and break accordingly
+      computer_selection = bot_selection
+      @player_2_turn.column_select(computer_selection)
+      #check win conditions and break accordingly
+    end
+  end
+
+
 
   def start
     main_menu_loop
-    player_1_turn = Turn.new(@player_1, @board)
-    player_2_turn = Turn.new(@player_2, @board)
+    player_turns
   end
 
 
