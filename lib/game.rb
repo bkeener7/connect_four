6d659358fb06f188402ec8493fe2eb1d3a7ba58a
Mostafa Sakr <msakr3@gmail.com>
2022-09-04 22:31:14 -0600

    Add tests for different helper methods created to bypass testing user input.

--- lib/game.rb
+++ lib/game.rb
@@ -1,23 +1,52 @@
 class Game 
   attr_reader :player_1,
-              :player_2
+              :player_2,
+              :board,
+              :bot_choices
 
   def initialize
     @player_1 = ""
     @player_2 = ""
+    @board = Board.new
+    @is_bot = false
+    @bot_choices = ["A", "B", "C", "D", "E", "F", "G"]
+  end
+
+  def set_player1
+    loop do
+      print "\nPlayer 1, please enter your name.\n"
+      @player1 = gets.chomp
+      @player1 != "" ? break : (print "\nSorry. Please try again.")
+    end
+  end
+
+  #Reuse above for setting player2 
+
+  def is_bot?
+    @is_bot
   end
 
   def set_bot
-    @player_2 ="Computer"
+    @is_bot = true
+    @player_2 = "Computer"
   end
 
-  def main_menu_message
+  def bot_selection
+    @bot_choices = @bot_choices.shuffle
+    @bot_choices[0]
+  end
+
+  def welcome_message
     "\nWelcome to CONNECT FOUR\nEnter p to play. Enter q to quit.\n"
   end
   #^there due to limitation on testing methods with user input.
 
   def main_menu
-    print main_menu_message
+    print welcome_message
+    main_menu_user_input
+  end
+
+  def main_menu_user_input
     if gets.chomp == "q"
       abort ":("
     elsif gets.chomp == "p"
@@ -30,33 +59,16 @@ class Game
   def main_menu_loop
     loop do
       main_menu
-      if @player1 != ""
-        break
-      end
     end
   end
 
-  def set_player1
-    loop do
-      print "\nPlayer 1, please enter your name.\n"
-      @player1 = gets.chomp
-      if @player1 != ""    #note to self test one line
-        break
-      else 
-        print "\nSorry. Please try again."
-      end
-    end
-  end
 
   def start
     main_menu_loop
-    # turn = Turn.new(user_input,player1, player2)
+    player_1_turn = Turn.new(@player_1, @board)
+    player_2_turn = Turn.new(@player_2, @board)
   end
 
-  # def set_player2
-  # print "\nPlayer 2, please enter your name.\n"
-  # @player2 = gets.chomp
-  # end
 
   # def choose_opponent_prompt
   #   "\nType 'Computer' to play against computer or 'Player' to play against another player\n"
