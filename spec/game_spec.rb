06096afa22a4dc50e577624a44d4e265b7b13439
Mostafa Sakr <msakr3@gmail.com>
2022-09-06 17:41:18 -0600

    Modify tests to pass after change to corresponding code.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,12 +13,6 @@ RSpec.describe Game do
     expect(game).to be_an_instance_of(Game)
   end
 
-  it "2. has two attributes: player1 and player2 that default to blank" do
-    game = Game.new
-    expect(game.player_1).to eq("")
-    expect(game.player_2).to eq("")
-  end
-
   it "3. has an instance of the Board class as an attribute" do
     game = Game.new
 
@@ -31,20 +25,11 @@ RSpec.describe Game do
     expect(game.is_bot?).to eq(false)
   end
 
-  it "5. can set player 2 to be a bot" do
-    game = Game.new
-
-    expect(game.is_bot?).to eq (false)
-    game.set_bot
-    expect(game.is_bot?).to eq (true)
-    expect(game.player_2).to eq("Computer")
-
-  end
 
   it "6. bot only has columns A through G as options to select" do
     game = Game.new
 
-    expect(game.bot_choices).to eq(["A", "B", "C", "D", "E", "F", "G"])
+    expect(game.column_choices).to eq(["A", "B", "C", "D", "E", "F", "G"])
   end
 
   it "7. has random bot selection by shuffling the choices A through G and always returning first element" do
@@ -52,22 +37,22 @@ RSpec.describe Game do
     bot_selection_1 = ""
     bot_selection_2 =""
     
-    expect(game.bot_choices).to eq(["A", "B", "C", "D", "E", "F", "G"])
+    expect(game.column_choices).to eq(["A", "B", "C", "D", "E", "F", "G"])
     game.bot_selection
-    expect(game.bot_choices).not_to eq(["A", "B", "C", "D", "E", "F", "G"]) #may possibly randomly stay the same but statistically unlikely. If it ever breaks just rerun it.
-    expect(game.bot_choices).to include("A", "B", "C", "D", "E", "F", "G")
-    bot_selection_1 = game.bot_choices[0]
+    expect(game.column_choices).not_to eq(["A", "B", "C", "D", "E", "F", "G"]) #may possibly randomly stay the same but statistically unlikely. If it ever breaks just rerun it.
+    expect(game.column_choices).to include("A", "B", "C", "D", "E", "F", "G")
+    bot_selection_1 = game.column_choices[0]
     game.bot_selection
-    expect(game.bot_choices).not_to eq(["A", "B", "C", "D", "E", "F", "G"])
-    expect(game.bot_choices).to include("A", "B", "C", "D", "E", "F", "G")
-    bot_selection_2 = game.bot_choices[0]
+    expect(game.column_choices).not_to eq(["A", "B", "C", "D", "E", "F", "G"])
+    expect(game.column_choices).to include("A", "B", "C", "D", "E", "F", "G")
+    bot_selection_2 = game.column_choices[0]
     expect(bot_selection_1).not_to eq(bot_selection_2) #Since it is a test for randomness, it can potentially fail. If you rerun it enough times, you should be able to get it to pass. This shows it is indeed random!
   end
 
 
   it "8. has a welcome message" do
     game = Game.new
-    expect(game.welcome_message).to eq("\nWelcome to CONNECT FOUR\nEnter p to play. Enter q to quit.\n")
+    expect(game.welcome_message).to eq("\nWelcome to CONNECT FOUR\nEnter 'P' to play. Enter 'QUIT' to exit at anytime.\n")
   end
 
   
